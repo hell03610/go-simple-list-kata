1df3139303fdeec696ed2912a48bb68305bd18b7
hell03610 <eraseunavez@gmail.com>
2014-06-30 20:23:02 +0100

    simple list

--- simplelist.go
+++ simplelist.go
@@ -1,36 +1,78 @@
 package main
 
 import (
-	//"errors"
+	"errors"
 )
 
 type List struct {
-
+    Head *Item
+    Last *Item
 }
 
 type Item struct {
-
+	Value string
+    Next *Item
 }
 
 func NewList()(*List){
-	return &List{}
+    return &List{}
 }
 
-func (l *List) Find(item string) (Item, error) {
-	//return Item {}, errors.New("empty")
-	return Item {} , nil
+func NewItem(item string)(*Item){
+	return &Item{Value: item, Next: nil}
+}
+
+func (l *List) Find(value string) (*Item, error) {
+    var found *Item
+    item := l.Head
+	for item != nil {
+		if(item.Value == value){
+			found = item
+			break
+		}
+		item = item.Next
+	}
+	if(found != nil){
+		return found, nil
+	}
+    return nil, errors.New("not found")
 }
 
 func (l *List) Add(item string) {
-
+   new_item :=NewItem(item)
+   if(l.Last == nil){
+       l.Last = new_item
+       l.Head = l.Last
+   }else{
+	  l.Last.Next = new_item //algo con punteros here
+      l.Last = new_item
+   }
 }
 
 func (l *List) Values() ([]string){
-	return nil
+	result := []string{}
+	item := l.Head
+	for item != nil {
+		result = append(result, item.Value)
+		item = item.Next
+	}
+	return result
 }
 
-func (l *List) Delete(Item) {
+func (l *List) Delete(item *Item) {
+	previous := l.previous(item)
+	if(previous != nil){
+		previous.Next = item.Next
+	}
+}
 
+func (l *List)previous(item *Item)(*Item){
+	for p:= l.Head; p != nil; p = p.Next {
+		if(p.Next == item){
+			return p
+		}
+	}
+	return nil
 }
 
 func main() {
--- simplelist_test.go
+++ simplelist_test.go
@@ -7,10 +7,41 @@ import (
 
 func TestEmptyList(t *testing.T) {
 
-	list := NewList()
+    list := NewList()
     _, err := list.Find("fred")
     if(err == nil){
 		t.Error("New list should be empty")
+    }
+}
+
+func TestAddValue(t *testing.T) {
+	list := NewList()
+	list.Add("One")
+
+	expected := "One"
+	item, _ := list.Find("One")
+
+    if(expected != item.Value){
+		t.Error("AddValues: Error in value")
+    }
+}
+
+func TestValues(t *testing.T) {
+	list := NewList()
+	list.Add("One")
+
+	expected := []string{"One"}
+	actual := list.Values()
+
+    if(len(expected) != len(actual)){
+		t.Error("AddValues: Length error")
+	}
+
+	for i, v := range actual {
+        //fmt.Printf("%d,%s, %s\n", i, v, expected[i])
+        if(expected[i] != v) {
+        	t.Error("AddValues: Error in value")
+        }
     }
 }
 
@@ -20,7 +51,7 @@ func TestAddValues(t *testing.T) {
 	list.Add("Two")
 	list.Add("Three")
 
-	expected := []string{"One","Two","Thee"}
+	expected := []string{"One","Two","Three"}
 	actual := list.Values()
 
 	if(len(expected) != len(actual)){
@@ -28,7 +59,6 @@ func TestAddValues(t *testing.T) {
 	}
 
 	for i, v := range actual {
-        //fmt.Printf("%d,%s, %s\n", i, v, expected[i])
         if(expected[i] != v) {
         	t.Error("AddValues: Error in value")
         }
@@ -41,11 +71,11 @@ func TestDeleteValues(t *testing.T) {
 	list.Add("Two")
 	list.Add("Three")
 
-	item, _ := list.Find("Two")
+ 	item, _ := list.Find("Two")
 
 	list.Delete(item)
 
-	expected := []string{"One","Thee"}
+	expected := []string{"One","Three"}
 	actual := list.Values()
 
 	if(len(expected) != len(actual)){
@@ -53,7 +83,6 @@ func TestDeleteValues(t *testing.T) {
 	}
 
 	for i, v := range actual {
-        //fmt.Printf("%d,%s, %s\n", i, v, expected[i])
         if(expected[i] != v) {
         	t.Error("AddValues: Error in value")
         }
